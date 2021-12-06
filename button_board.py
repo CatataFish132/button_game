a41df775af407a8d9c9f5d7b80ae3d597bd59cc1
thimo <t.veldema@st.hanze.nl>
2021-12-06 11:27:38 +0100

    added button func

--- button_board.py
+++ button_board.py
@@ -3,10 +3,12 @@ from pydub.playback import play
 try:
     import board
     import neopixel
-except NotImplementedError:
+    import digitalio
+except (NotImplementedError, ModuleNotFoundError):
     print("running in emulation mode")
     import neopixel_emu as neopixel
     import mockboard as board
+    import digitalio
 import time
 import asyncio
 import threading
@@ -19,17 +21,32 @@ class ButtonBoard:
         self.running = True
         self.config = configparser.ConfigParser()
         self.config.read("config.ini")
-        # if its not running on a pi this will emulate what the NeoPixel library does
-        # try:
         self.speaker = self.Speakers()
         self.serial = serial.Serial(self.config["serial"]["port"], int(self.config["serial"]["speed"]))
 
         # Creating the buttons
         self.buttons = []
+        x = int(self.config["board"]["size_x"])-1
+        y = int(self.config["board"]["size_y"])-1
         for i in range(int(self.config["board"]["size_y"])):
             self.buttons.append([])
             for j in range(int(self.config["board"]["size_x"])):
-                self.buttons[i].append(self.Button(self, i*6 + j))
+                self.buttons[i].append(self.Button(self, i*y + j))
+
+        # creating pins
+        x_pins = []
+        for i in range(int(self.config["board"]["size_x"])):
+            x_pins.append(getattr(board, f"D{i+1}"))
+        y_pins = []
+        y_size = int(self.config["board"]["size_y"])
+        for i in range(y_size):
+            y_pins.append(getattr(board, f"D{i+y_size+1}"))
+        for x_pin in x_pins:
+            self.x_pins.append(digitalio.DigitalInOut(x_pin))
+            self.x_pins[-1].direction = digitalio.Direction.OUTPUT
+        for y_pin in y_pins:
+            self.y_pins.append(digitalio.DigitalInOut(y_pin))
+            self.y_pins[-1].direction = digitalio.Direction.INPUT
         t1 = threading.Thread(target=self.thread_loop, args=(), daemon=True)
         t1.start()
 
@@ -67,14 +84,18 @@ class ButtonBoard:
             sound = AudioSegment.from_mp3('sounds/' + filename)
             t = threading.Thread(target=f, args=(sound,))
             t.start()
-            # play(sound)
 
     # loop for detecting button presses.
     # TODO: add ability to detect button presses
     def thread_loop(self):
-        print("its working i guess")
         while self.running:
-            time.sleep(2)
+            for i, y_pin in enumerate(self.y_pins):
+                y_pin.value = True
+                for j, x_pin in enumerate(self.x_pins):
+                    if x_pin.value:
+                        self.buttons[i][j].pressed = True
+                    else:
+                        self.buttons[i][j].pressed = False
 
     def colour_all_leds(self, colour=(254, 254, 254)):
         for button_row in self.buttons:
